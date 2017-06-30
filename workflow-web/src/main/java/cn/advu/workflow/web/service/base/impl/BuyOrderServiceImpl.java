dc42fca2df214f080a44e5ccd4194632732657b6
wangrya@chanjet.com <wangrya@chanjet.com>
2017-07-01 00:01:12 +0800

    采购单调整完毕

--- workflow-web/src/main/java/cn/advu/workflow/web/service/base/impl/BuyOrderServiceImpl.java
+++ workflow-web/src/main/java/cn/advu/workflow/web/service/base/impl/BuyOrderServiceImpl.java
@@ -2,6 +2,11 @@ package cn.advu.workflow.web.service.base.impl;
 
 import cn.advu.workflow.dao.fcf_vu.BaseBuyOrderMapper;
 import cn.advu.workflow.domain.fcf_vu.BaseBuyOrder;
+import cn.advu.workflow.domain.fcf_vu.BaseExecuteOrder;
+import cn.advu.workflow.repo.fcf_vu.BaseBuyOrderRepo;
+import cn.advu.workflow.repo.fcf_vu.BaseExecuteOrderRepo;
+import cn.advu.workflow.web.common.ResultJson;
+import cn.advu.workflow.web.common.constant.WebConstants;
 import cn.advu.workflow.web.service.base.BuyOrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,30 +23,40 @@ public class BuyOrderServiceImpl implements BuyOrderService {
     private static final Logger LOGGER = LoggerFactory.getLogger(BuyOrderServiceImpl.class);
 
     @Autowired
-    private BaseBuyOrderMapper baseBuyOrderMapper;
+    private BaseBuyOrderRepo baseBuyOrderRepo;
 
     @Override
-    public int insert(BaseBuyOrder obj) {
-        return 0;
+    public ResultJson<List<BaseBuyOrder>> findAll() {
+        ResultJson<List<BaseBuyOrder>> result = new ResultJson<>(WebConstants.OPERATION_SUCCESS);
+        result.setData(baseBuyOrderRepo.findAll());
+        return result;
     }
 
     @Override
-    public int delete(String id) {
-        return 0;
+    public ResultJson<Integer> add(BaseBuyOrder baseBuyOrder) {
+        Integer insertCount = baseBuyOrderRepo.addSelective(baseBuyOrder);
+        if(insertCount != 1){
+            return new ResultJson<>(WebConstants.OPERATION_FAILURE, "创建采购单失败!");
+        }
+        return new ResultJson<>(WebConstants.OPERATION_SUCCESS);
     }
 
     @Override
-    public int update(String id) {
-        return 0;
+    public ResultJson<Integer> update(BaseBuyOrder baseBuyOrder) {
+        if (baseBuyOrder.getId() == null) {
+            return new ResultJson<>(WebConstants.OPERATION_FAILURE, "ID没有设置!");
+        }
+        Integer insertCount = baseBuyOrderRepo.updateSelective(baseBuyOrder);
+        if(insertCount != 1){
+            return new ResultJson<>(WebConstants.OPERATION_FAILURE, "更新采购单失败!");
+        }
+        return new ResultJson<>(WebConstants.OPERATION_SUCCESS);
     }
 
     @Override
-    public List<BaseBuyOrder> queryAll() {
-        return null;
-    }
-
-    @Override
-    public List<BaseBuyOrder> queryByCondition(BaseBuyOrder obj) {
-        return null;
+    public ResultJson<BaseBuyOrder> findById(Integer id) {
+        ResultJson<BaseBuyOrder> result = new ResultJson<>(WebConstants.OPERATION_SUCCESS);
+        result.setData(baseBuyOrderRepo.findOne(id));
+        return result;
     }
 }
