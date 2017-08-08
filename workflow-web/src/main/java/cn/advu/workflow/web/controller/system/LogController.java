d298627ae3345ba16ef0e19b5693c363d5cd18c7
qpzmal <qpzmal@gmail.com>
2017-08-08 22:14:16 +0800

    在测试环境，日志页面404

--- workflow-web/src/main/java/cn/advu/workflow/web/controller/system/LogController.java
+++ workflow-web/src/main/java/cn/advu/workflow/web/controller/system/LogController.java
@@ -1,38 +1,21 @@
 package cn.advu.workflow.web.controller.system;
 
-import cn.advu.workflow.domain.fcf_vu.*;
-import cn.advu.workflow.web.common.ResultJson;
-import cn.advu.workflow.web.common.constant.WebConstants;
-import cn.advu.workflow.web.common.loginContext.LoginAccount;
-import cn.advu.workflow.web.common.loginContext.LoginUser;
-import cn.advu.workflow.web.common.loginContext.UserThreadLocalContext;
-import cn.advu.workflow.web.dto.system.UserRole;
+import cn.advu.workflow.domain.fcf_vu.SysLogWithBLOBs;
 import cn.advu.workflow.web.manager.BizLogManager;
-import cn.advu.workflow.web.service.system.LoginService;
-import cn.advu.workflow.web.service.system.SysRoleService;
-import cn.advu.workflow.web.service.system.SysUserService;
 import cn.advu.workflow.web.util.AssertUtil;
-import cn.advu.workflow.web.vo.MenuVO;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.google.gson.Gson;
-import net.sf.ehcache.search.parser.MCriteria;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 @Controller
-@RequestMapping("log")
+@RequestMapping("/sysLog")
 public class LogController {
 
     private static Logger LOGGER = LoggerFactory.getLogger(LogController.class);
